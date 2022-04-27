f4b3b5adc8000e344c3d23daf37fb4bdf3b4f5a1
Basheer Akbari <basheer.akbari@gmail.com>
2022-04-27 17:29:52 -0400

    linking hobby name to profile

--- src/user/Profile.jsx
+++ src/user/Profile.jsx
@@ -17,16 +17,19 @@ export default function Profile  (props)  {
 
     useEffect(()=>{
         // console.log('useEffected')
-        // Axios.get(`profile/?id=${props.user.id}`)
-        Axios.get(`profile/?id=626922754021f5fd2e27997b`)
-        .then(response=>{
-            setProfile(response.data)
-            console.log(response)
-        })
-        .catch(error=>{
-            console.log(error)
-        })
-    },[])
+        console.log(props.user.id)
+        if(props.user.id){   
+            Axios.get(`profile/?id=${props.user.id}`)
+            // Axios.get(`profile/?id=626922754021f5fd2e27997b`)
+            .then(response=>{
+                setProfile(response.data)
+                console.log(response)
+            })
+            .catch(error=>{
+                console.log(error)
+            })
+        }
+    },[props.user.id])
     
     // useEffect(()=>{
     //     console.log('useEffect2',profileState)
@@ -89,7 +92,7 @@ export default function Profile  (props)  {
                                             <ul style={{  listStyleType: 'none'}}>
                                                {
                                                     profileState.hobby.map(function(hobby){
-                                                    return <li key={hobby}> {hobby} is my hobby </li>
+                                                    return <li key={hobby.id}> {hobby.name} is my hobby </li>
                                                     })
                                                 }
                                             </ul>
@@ -103,9 +106,9 @@ export default function Profile  (props)  {
                         <h1>Events!</h1>
                         <ul style={{  listStyleType: 'none'}}>
                             {
-                                profileState.events.map(function(event){
+                                profileState.events.map(function(event,index){
                                     return (
-                                        <div style={{ display: "block", margin:'2vh auto',maxHeight:'20vh', maxWidth:'50vw',minWidth:'25vw'}} >
+                                        <div key={index} style={{ display: "block", margin:'2vh auto',maxHeight:'20vh', maxWidth:'50vw',minWidth:'25vw'}} >
                                             <div style={{height:'100%', width:'100%', border:'1px solid gray', margin:'0 auto',border:'2px solid gray'}}>
                                                 {event}
                                                 <Image fluid style={{objectFit:'cover', padding:0 }}  src='https://imgs.search.brave.com/m1NcZ4LFZIcTN6zGwTI6fnMXVAAO2WjYrDDDyF2Bcb0/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9zdGF0/aWMzLnNyY2RuLmNv/bS93b3JkcHJlc3Mv/d3AtY29udGVudC91/cGxvYWRzLzIwMTcv/MDIvZGFuaWVsLXJh/ZGNsaWZmZS13b3Vs/ZC10aGluay1hYm91/dC1oYXJyeS1wb3R0/ZXItcmV0dXJuLmpw/Zw'/>
