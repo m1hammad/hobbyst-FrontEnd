2755e0b8e9e2d32c34912ca2e60d69843f54d51f
apimento <98665437+apimento@users.noreply.github.com>
2022-04-28 11:34:27 -0400

    some styling changes

--- src/Profile/ProfileEdit.jsx
+++ src/Profile/ProfileEdit.jsx
@@ -1,110 +1,110 @@
-import Axios from 'axios';
-import React, { useState, useEffect} from 'react'
-import {Container, Button, Row, Col,Form } from "react-bootstrap"
-import { useNavigate } from "react-router";
-import './Profile.css';
-import {useParams} from "react-router-dom" 
+// import Axios from 'axios';
+// import React, { useState, useEffect} from 'react'
+// import {Container, Button, Row, Col,Form } from "react-bootstrap"
+// import { useNavigate } from "react-router";
+// import './Profile.css';
+// import {useParams} from "react-router-dom" 
 
-import Select from 'react-select'
+// import Select from 'react-select'
 
 
-export default function ProfileEdit(props){
+// export default function ProfileEdit(props){
 
-    // const [user, setUser] = useState(null);
-    const [profileState, setProfile] = useState(null)
+//     // const [user, setUser] = useState(null);
+//     const [profileState, setProfile] = useState(null)
 
-    // const [hobbyState, setHobby] = useState([])
+//     // const [hobbyState, setHobby] = useState([])
 
-    const navigate = useNavigate();
-    const params = useParams(); 
-    console.log(params.id,'params.id')  
+//     const navigate = useNavigate();
+//     const params = useParams(); 
+//     console.log(params.id,'params.id')  
 
-    // console.log(props,'props')
+//     // console.log(props,'props')
 
-    useEffect(()=>{
-        // console.log('useEffected')
-        console.log(props.user.id)
-        if(props.user.id){   
-            Axios.get(`/api/auth/user/${params.id}`)
-            .then(response=>{
-                setProfile(response.data)
-                console.log(response.data)
-            })
-            .catch(error=>{
-                console.log(error)
-            })
-        }
-    },[props.user.id])
+//     useEffect(()=>{
+//         // console.log('useEffected')
+//         console.log(props.user.id)
+//         if(props.user.id){   
+//             Axios.get(`/api/auth/user/${params.id}`)
+//             .then(response=>{
+//                 setProfile(response.data)
+//                 console.log(response.data)
+//             })
+//             .catch(error=>{
+//                 console.log(error)
+//             })
+//         }
+//     },[props.user.id])
 
-    console.log(props.user.id)
+//     console.log(props.user.id)
 
-    const changeHandler = (e) => {
-        let temp = {...user}
-        temp[e.target.name] = e.target.value
-        temp.hobby = hobbyState
-        console.log("this is temp", temp)
-        setUser(temp)
-    }
+//     const changeHandler = (e) => {
+//         let temp = {...user}
+//         temp[e.target.name] = e.target.value
+//         temp.hobby = hobbyState
+//         console.log("this is temp", temp)
+//         setUser(temp)
+//     }
    
-    const editHandler = async() => {
-        // props.register(user)
-        // console.log("hobby state", {hobbyState})
-        // console.log("user", user)
-        await Axios.post(`/profile/edit/${props.user.id}`)
-        navigate('/profile')
-    }
-    const hobbyArr = props.hobbies.map((hobby, index) => (
-        <Button name='hobby' type='button' className='hobbyBtns' key={index} onClick={selectHobby} value={hobby._id} multiple >{hobby.name}</Button> 
-        )
-        )
+//     const editHandler = async() => {
+//         // props.register(user)
+//         // console.log("hobby state", {hobbyState})
+//         // console.log("user", user)
+//         await Axios.post(`/profile/edit/${props.user.id}`)
+//         navigate('/profile')
+//     }
+//     const hobbyArr = props.hobbies.map((hobby, index) => (
+//         <Button name='hobby' type='button' className='hobbyBtns' key={index} onClick={selectHobby} value={hobby._id} multiple >{hobby.name}</Button> 
+//         )
+//         )
 
-    return(
+//     return(
 
-        <div>
-        <h1>Edit your profile</h1>
-        <Button variant='primary' href={`/profile/delete/${props.user.id}`}>Delete profile</Button>
+//         <div>
+//         <h1>Edit your profile</h1>
+//         <Button variant='primary' href={`/profile/delete/${props.user.id}`}>Delete profile</Button>
 
-        <Container>
-            <Form.Group>
-                <Form.Label>First Name</Form.Label>
-                <Form.Control name="firstName" onChange={changeHandler}></Form.Control>
-            </Form.Group>
+//         <Container>
+//             <Form.Group>
+//                 <Form.Label>First Name</Form.Label>
+//                 <Form.Control name="firstName" onChange={changeHandler}></Form.Control>
+//             </Form.Group>
 
-            <Form.Group>
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control name="lastName" onChange={changeHandler}></Form.Control>
-            </Form.Group>
+//             <Form.Group>
+//                 <Form.Label>Last Name</Form.Label>
+//                 <Form.Control name="lastName" onChange={changeHandler}></Form.Control>
+//             </Form.Group>
 
-            <Form.Group>
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control name="emailAddress" onChange={changeHandler}></Form.Control>
-            </Form.Group>
+//             <Form.Group>
+//                 <Form.Label>Email Address</Form.Label>
+//                 <Form.Control name="emailAddress" onChange={changeHandler}></Form.Control>
+//             </Form.Group>
 
-            <Form.Group>
-                <Form.Label>City</Form.Label>
-                <Form.Control name="city" onChange={changeHandler}></Form.Control>
-            </Form.Group>
+//             <Form.Group>
+//                 <Form.Label>City</Form.Label>
+//                 <Form.Control name="city" onChange={changeHandler}></Form.Control>
+//             </Form.Group>
 
-            <Form.Group>
-                <Form.Label>Province</Form.Label>
-                <Form.Control name="province" onChange={changeHandler}></Form.Control>
-            </Form.Group>
+//             <Form.Group>
+//                 <Form.Label>Province</Form.Label>
+//                 <Form.Control name="province" onChange={changeHandler}></Form.Control>
+//             </Form.Group>
                 
             
-            <Form.Group>
-                <Form.Label>About</Form.Label>
-                <Form.Control name="about" onChange={changeHandler}></Form.Control>
-            </Form.Group>
+//             <Form.Group>
+//                 <Form.Label>About</Form.Label>
+//                 <Form.Control name="about" onChange={changeHandler}></Form.Control>
+//             </Form.Group>
 
-             <Form.Group>
-                <Form.Label>Hobbies:</Form.Label> <br/>
-                    {hobbyArr}
-            </Form.Group> 
+//              <Form.Group>
+//                 <Form.Label>Hobbies:</Form.Label> <br/>
+//                     {hobbyArr}
+//             </Form.Group> 
            
 
-            <Button variant='primary' onClick={editHandler} >Save Changes</Button>
+//             <Button variant='primary' onClick={editHandler} >Save Changes</Button>
 
-        </Container>
-      </div>
-    )
-}
+//         </Container>
+//       </div>
+//     )
+// }
--- src/event/EventDetail.jsx
+++ src/event/EventDetail.jsx
@@ -2,6 +2,36 @@ import React from 'react'
 
 export default function EventDetail() {
   return (
-    <div>EventDetail</div>
+    <div>
+        <Container fluid style={{textAlign:'center', margin:'2vh auto',padding:'0', backgroundColor:'white',borderRadius:'10px',
+                                                boxShadow: '4px 4px 10px gray'}} key={index}>
+                                                <h3>{event.title}</h3>
+                                                <Row style={{height:'100%', width:"max-content",maxWidth:'90vw', margin:'0 auto',padding:'0em', justifyContent:'space-between'}}>
+                                                    <Col sm>
+                                                        <Image fluid style={{objectFit:'cover', padding:0, width:'300px', borderRadius:'10px'}}  src={`${event.photo}`}/>
+                                                        <p>{event.description}</p>
+                                                    </Col>
+                                                    <Col sm>
+                                                        <div style={{display:'flex',justifyContent:'center' }}>
+                                                            <iframe 
+                                                                title={`${event.title}map`}
+                                                                src= {`https://maps.google.com/maps?q=${event.preciseLocation}=&output=embed`}
+                                                                width="275"
+                                                                height="200"
+                                                                frameBorder="0"
+                                                                scrolling="no"
+                                                                marginHeight="0"
+                                                                marginWidth="0"
+                                                                >
+                                                            </iframe>
+                                                        </div>
+
+                                                    </Col>
+                                                    {/* <Button>Edit Event</Button> */}
+                                                </Row>
+                                            </Container>
+
+
+    </div>
   )
 }
