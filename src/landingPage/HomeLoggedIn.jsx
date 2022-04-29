b8789c33636a7a5d0bd476c86b59df635f0b25cc
apimento <98665437+apimento@users.noreply.github.com>
2022-04-28 20:43:43 -0400

    event details container working and join button added

--- src/landingPage/HomeLoggedIn.jsx
+++ src/landingPage/HomeLoggedIn.jsx
@@ -23,8 +23,8 @@ export default function HomeLoggedIn(props) {
   return ( 
     <>
     <div>
-      <h1>Welcome Back, <span id='username'>{props.user.name}</span> </h1>  
       <h1 className="appLogo"> HOBBYST </h1>
+      <h1>Welcome Back, <span id='username'>{props.user.name}</span> </h1>  
       {/* {hobbies.map()} */}
     </div>
   </>
